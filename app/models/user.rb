837939031e5c8eb23b03b649c66499a9f2621bd9
Naveen Kumar <naveen.kumar@anchanto.com>
2024-02-28 13:22:51 +0530

    added

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          belongs_to :clinic
          has_many :appointments, foreign_key: 'user_id'
          has_many :doctor_appointments, class_name: 'Appointment', foreign_key: 'doctor_id'
-         enum role: { doctor: 'doctor', receptionist: 'receptionist', medicine_maker: 'medicine_maker' }
+         enum role: { doctor: 0, receptionist: 1, medicine_maker: 2, admin: 3 }
          scope :doctors, -> { where(role: :doctor) }
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Wms::User.create({"email"=>"admin@rn.com", "role"=>3, "clinic_id"=>2, "name"=>"admin"})
