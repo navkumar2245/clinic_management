7088af953bedb15ada17870de999bbaabd0bcac8
Naveen Kumar <naveen.kumar@anchanto.com>
2024-03-01 23:36:38 +0530

    fixed the screen size issue

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': '360px',   // Extra small devices (phones)
+        'sm': '640px',   // Small devices (tablets)
+        'md': '768px',   // Medium devices (landscape tablets)
+        'lg': '1024px',  // Large devices (desktops)
+        'xl': '1280px',  // Extra large devices (large desktops)
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
